14bb1e616885b008c7abdfc1cb5d6cc5a2e504f2
githukelvin <kelvingithu019@gmail.com>
2023-06-13 15:33:08 +0300

    added netfliy plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown:{
-    drafts:true
+  markdown: {
+    drafts: true
   },
-  integrations: [vue(), mdx()]
+  integrations: [vue(), mdx()],
+  output: "server",
+  adapter: netlify()
 });
